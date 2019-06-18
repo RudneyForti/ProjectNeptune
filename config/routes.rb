2bef4279659768688bb485bff6452c9514aa1d9c
Rudney Forti <rudneyforti@hotmail.com>
2019-06-18 14:19:01 +0000

    Login Link Fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  
+  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'index#inicio'
   get "/inicio" => "index#inicio"
   
-  #get '/login' to: 'devise/sessions#new'
+ # get '/login' to: 'devise/sessions#new'
   
   
   get "/dash" => "dashboard#dash"
   
   
-  devise_for :users, :controllers => { registrations: 'users/registrations', sessions: "users/sessions", passwords: 'users/passwords' } do
+  devise_for :users => { registrations: 'users/registrations', sessions: "users/sessions", passwords: 'users/passwords' } do
     get "/", :to => "users/sessions#create"
   end
 
@@ -20,7 +22,7 @@ Rails.application.routes.draw do
 
   unauthenticated :user do
     devise_scope :user do
-      get "/" => "users/sessions#new"
+      get "/login" => "users/sessions#new"
     end
   end
   
