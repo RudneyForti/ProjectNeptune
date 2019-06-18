7581d51f2e22a1f7af1936aaabe3acad85242541
Rudney Forti <rudneyforti@hotmail.com>
2019-06-18 11:30:39 +0000

    Devise Login Created, Routes Fixed

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,6 @@
 class IndexController < ApplicationController
+    
+    def inicio
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'index#inicio'
+  get "/inicio" => "index#inicio"
+  
+  get '/login' to: 'devise/sessions#new'
+  
+  
+  get "/dash" => "dashboard#dash"
+  
+  
+  devise_for :users, :controllers => { registrations: 'users/registrations', sessions: "users/sessions", passwords: 'users/passwords' } do
+    get "/", :to => "users/sessions#create"
+  end
+
+  authenticated :user do
+  end
+
+  unauthenticated :user do
+    devise_scope :user do
+      get "/" => "users/sessions#new"
+    end
+  end
+  
 end
